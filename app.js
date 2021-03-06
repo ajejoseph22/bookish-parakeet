befd697a8a7cf1fede1bebbdeb209018eabd669a
Joseph Aje <ajejoseph22@gmail.com>
2021-03-06 16:53:37 +0100

    chore: deleted unneeded routes

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-require('dotenv').config()
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+require("dotenv").config();
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const emailRoute = require('./routes/email');
+const emailRoute = require("./routes/email");
 const cors = require("cors");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/email', emailRoute);
+app.use("/email", emailRoute);
 
 module.exports = app;
