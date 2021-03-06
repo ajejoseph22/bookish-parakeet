fe8d95c3e1800244fcae7f2ac59aa0689836396b
Joseph Aje <ajejoseph22@gmail.com>
2021-03-06 14:02:57 +0100

    chore: removed public folder from app.js

--- app.js
+++ app.js
@@ -11,9 +11,8 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
